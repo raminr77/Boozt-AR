269bed2e57248ff4f52cf0faf4e2f0073dad8368
Ramin Rezaei <rare@boozt.com>
2024-05-08 09:16:06 +0200

    feat: init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Boozt AR
+
+Developer: [Ramin](https://raminrezaei.se)
+<br />
+Augmented Reality Project For [Boozt.com](https://boozt.com)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='min-h-screen bg-slate-50 flex items-center justify-center'>
+      Hello
+    </main>
+  );
+}
+
+export default App;
