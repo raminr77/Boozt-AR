4688167c6af8026c950222472436156a98ad3098
Ramin Rezaei <rare@boozt.com>
2024-05-15 02:05:35 +0200

    fix: add face tracking

--- src/components/face-tacking.tsx
+++ src/components/face-tacking.tsx
@@ -1,3 +1,13 @@
+const URL = 'https://raminrezaei.ir/face-landmark-detection/';
+
 export function FaceTacking() {
-  return <section>Face Tracking</section>;
+  return (
+    <section className='w-11/12 max-w-screen-sm overflow-hidden flex flex-col gap-4 items-center'>
+      <div className='w-full h-96 bg-slate-300 rounded overflow-hidden relative'>
+        {/* Face Tracking */}
+        <iframe src={URL} width='100%' height='100%' allow='camera; microphone'></iframe>
+        {/* Face Tracking */}
+      </div>
+    </section>
+  );
 }
